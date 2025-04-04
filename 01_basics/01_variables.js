ba4138b6709ff55f2e625c27726c077241e13561
pritish157 <125906918+pritish157@users.noreply.github.com>
2025-04-04 11:15:08 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountemail = "pritish@gmail.com"
+var accountpassword = "12345"
+accountcity ="jaipur"
+let accountstate;
+//accountId = 2// not allowed as constant value/operator cant be changed further
+
+accountemail ="hc@hc.com"
+accountpassword = "121212"
+accountcity = "bhadrak"
+console.log(accountId);
+ console.table([accountId,accountemail,accountpassword,accountcity,accountstate])
+ /* 
+ prefer not to use var
+ because of issue in block scope and functional scope
+ */
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js code as newer version
+// alert("hello")//we are using nodejs not browser
+console.log(3+3) 
+// code readability must be high
+console.log("pritish") 
+let name ="pritish"
+let age ="20"
+let isloggedin = false
+
+//number =>2^53
+//big int
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>value not assigned
+//symbol=>used for uniqueness
+
+
+//object
+
+
+console.log(typeof null)//output is object
+console.log(typeof undefined)//output is undefined
+
