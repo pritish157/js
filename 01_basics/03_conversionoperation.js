a0317a3fe7376a8c7fdd465df49e59fa9eb4bb61
pritish157 <125906918+pritish157@users.noreply.github.com>
2025-04-14 10:29:36 +0000

    var.and datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,27 @@
+// let score =false
+// console.log(typeof(score))
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))//score contains a non numerical value yet the output says type of valueinnumber which is the container(variable) of score is NUMBER
+// console.log(valueInNumber)//when we are going to print the valueinnumber it says NaN(not a number) but previously it said type of valueinnumber is a number but out said not a number so the number sysytem is less reliable in javascript
+
+
+// "33"==> 33
+// "33abc"==>NaN
+// true==>1
+//false==>0
+// null==>0
+//undefined==>nan
+//pk==>error
+
+let A = "0"
+let b = Boolean(A)
+console.log(typeof A)
+console.log(typeof b)
+console.log(b)//conversion of boolean
+//1,"hitesh","0"==>true
+//0,null,undefined,""==>false
+
+
+
+
